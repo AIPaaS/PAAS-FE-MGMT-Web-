040f45fd8d91127c6b4dc730ab4fea77ade34a1c
wanwb <wanwb@asiainfo.com>
2016-03-07 12:14:14 +0800

    修改弹出DEV相对位置

---
+++ src/main/webapp/test/js/test.js
@@ -0,0 +1,39 @@
+
+/** 初始化 **/
+function init() {
+	initData(function() {
+		initComponent();
+		initListener();
+		initFace();
+	});
+}
+
+/** 初始化页面、内存等基本数据 **/
+function initData(cb) {
+	var a = PRQ.get("a");
+	
+	var ddd = 1;
+	
+	
+	
+	
+	if(CU.isFunction(cb)) cb();
+}
+
+/** 初始化组件 **/
+function initComponent() {
+}
+
+/** 对组件设置监听 **/
+function initListener() {
+}
+
+/** 初始化界面 **/
+function initFace() {
+}
+
+
+
+
+
+
---
+++ src/main/webapp/test/test.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html; charset=utf-8"%>
+<%@page import="com.binary.core.util.BinaryUtils"%>
+
+<%
+String ContextPath = request.getContextPath();
+%>
+
+
+
+<jsp:include page="/layout/jsp/head.jsp"></jsp:include>
+
+
+<div>
+aaaaaaaaaaaaaaaaaaa
+</div>
+
+
+<jsp:include page="/layout/jsp/footer.jsp"></jsp:include>
+
+
+
+
+
